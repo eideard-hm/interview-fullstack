7e5d2c4a23c0636a0e0f1ffb1170a9adf573b281
eideard-hm <ehhernandez81@misena.edu.co>
2022-05-15 16:53:10 -0500

    Proyectos de angular y .net para la aplicación a la vacante. Initial commit

---
+++ InventoryApp/Inventory.Infrastructure/Repository/InvoiceDetailRepository.cs
@@ -0,0 +1,44 @@
+﻿using Inventory.Domain.Entities;
+using Inventory.Domain.Interfaces.Repository;
+using Inventory.Infrastructure.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace Inventory.Infrastructure.Repository
+{
+    public class InvoiceDetailRepository : IInvoiceDetailRepository<InvoiceDetail, int>
+    {
+        private readonly InvoiceContext _db;
+        public InvoiceDetailRepository(InvoiceContext db)
+        {
+            _db = db;
+        }
+
+        public InvoiceDetail Add(InvoiceDetail invoiceDetail)
+        {
+            _db.InvoicesDetails.Add(invoiceDetail);
+            return invoiceDetail;
+        }
+
+        public List<InvoiceDetail> GetAll()
+        {
+            return _db.InvoicesDetails
+                                      .AsNoTracking()
+                                      .Include(id => id.Invoice)
+                                      .Include(id => id.Product)
+                                      .ToList();
+        }
+
+        public InvoiceDetail GetById(int id)
+        {
+            return _db.InvoicesDetails
+                                      .AsNoTracking()
+                                      .Include(id => id.InvoiceId)
+                                      .FirstOrDefault();
+        }
+
+        public void SaveAllChanges()
+        {
+            _db.SaveChanges();
+        }
+    }
+}
